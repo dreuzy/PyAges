d7631c8cd29c79847f7da24aa59b2075ae920201
Jean-Raynald de Dreuzy <Jean-Raynald.de-Dreuzy@univ-rennes1.fr>
2026-09-09 05:21:33 +0200

    ci: exclude bot-blocked Zenodo DOI checks

--- docs/conf.py
+++ docs/conf.py
@@ -133,6 +133,7 @@ linkcheck_ignore = [
     r"https://doi\.org/10\.15138/TQ02-ZX42",
     r"https://doi\.org/10\.17632/p32bmw6rgs\.1",
     r"https://doi\.org/10\.3133/tm4F3",
+    r"https://doi\.org/10\.5281/zenodo\.2215086[23]",
     r"https://hplus\.ore\.fr/en/ploemeur/",
     r"https://www\.usgs\.gov/software/tracerlpm",
     r"https://pubs\.usgs\.gov/publication/tm4F3",
